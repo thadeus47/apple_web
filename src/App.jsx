941f8d4f4b8e7312af5ead26c3aa5799ded3d142
m_thado <tmiseda@gmail.com>
2024-09-26 10:32:17 -0400

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
 
 
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import Highlights from './components/Highlights'
+
+
 const App = () => {
 
 
   return (
-    <>
-      <div>
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-      </div>
-     
-    </>
+    <main className="bg-black">
+      <Navbar />
+      <Hero />
+      <Highlights />
+    </main>
   )
 }
 
