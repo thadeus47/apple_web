932fa762269e3b9f5622167258be21c1118a1118
m_thado <tmiseda@gmail.com>
2024-09-30 17:51:02 -0400

    3d model view

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import Highlights from './components/Highlights'
+import Model from './components/Model'
 
 
 const App = () => {
@@ -13,6 +14,7 @@ const App = () => {
       <Navbar />
       <Hero />
       <Highlights />
+      <Model />
     </main>
   )
 }
