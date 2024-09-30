20f69b2a9d01afe4dc3e4d76dc48f071f36ad945
m_thado <tmiseda@gmail.com>
2024-09-30 19:55:07 -0400

    3d model view update

--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -15,7 +15,7 @@ const Model = () => {
 
     const [size, setSize] = useState('small');
     const [model, setModel] = useState({
-        tittle: 'iPhone 15 Pro in Natural Titanium',
+        title: 'iPhone 15 Pro in Natural Titanium',
         color: ["#8F8A81", "#ffe7b9", "#6f6c64"],
         img: yellowImg,
     })
@@ -80,7 +80,7 @@ const Model = () => {
                     </Canvas>
                 </div>
                  <div className="mx-auto w-full">
-                    <p className='text-sm font-light text-center mb-5'>{model.tittle}</p>
+                    <p className='text-sm font-light text-center mb-5'>{model.title}</p>
                     <div className="flex-center">
                         <ul className="color-container">
                             {models.map((item, i) => (
