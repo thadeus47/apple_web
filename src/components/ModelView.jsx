d11af7ceed1c2024013e01b4441abcecb4c83500
m_thado <tmiseda@gmail.com>
2024-10-01 19:45:28 -0400

    model view lights

--- src/components/ModelView.jsx
+++ src/components/ModelView.jsx
@@ -1,9 +1,16 @@
-
-const ModelView = () => {
+import { PerspectiveCamera, View } from "@react-three/drei"
+const ModelView = ({ index, groupRef, gsapType, controlRef,setRotationSize, size, item }) => {
   return (
-    <div>
-      ModelView
-    </div>
+     <View
+        index={index}
+        id={gsapType}
+        className={`border-2 border-red-500 w-full h-full ${index === 2} ? 'right-[-100%] : ''`}
+     > 
+     {/* Ambient Lights */}
+     <ambientLight intensity={0.3} />
+
+     <PerspectiveCamera makeDefault position={[0, 0, 4 ]}/>
+     </View>
   )
 }
 
