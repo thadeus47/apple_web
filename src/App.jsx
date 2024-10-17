75e80a9009d03e1a987bc684b7975ed0745d36f2
m_thado <tmiseda@gmail.com>
2024-10-17 11:38:50 -0400

    features section

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,20 @@ import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import Highlights from './components/Highlights'
 import Model from './components/Model'
-
+import Features from './components/Features';
+import HowItWorks from './components/HowItWorks';
 import * as Sentry from '@sentry/react';
 const App = () => {
 
-
+  
   return (
     <main className="bg-black">
       <Navbar />
       <Hero />
       <Highlights />
       <Model />
+      <Features />
+      <HowItWorks />
     </main>
   )
 }
