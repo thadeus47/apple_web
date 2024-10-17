fd3be745c390665fd495f145a3cf2e1fb44351e6
m_thado <tmiseda@gmail.com>
2024-10-17 12:35:13 -0400

    footer section update

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Highlights from './components/Highlights'
 import Model from './components/Model'
 import Features from './components/Features';
 import HowItWorks from './components/HowItWorks';
-import Footer from './components/Footer';
+import Footer from './components/Footer'; 
 import * as Sentry from '@sentry/react';
 const App = () => {
 
